3b15d63f3f752892eab91be091f764bea7d6a75c
IndraY97 <indrayulianto111@gmail.com>
2020-01-06 23:26:11 +0700

    Menambahkan Fitur Toast pada Button, untuk menampilkan info singkat

--- app/src/main/java/com/example/latihanbutton/MainActivity.java
+++ app/src/main/java/com/example/latihanbutton/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.latihanbutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1,btn2,btn5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1= (Button)findViewById(R.id.button1);
+        btn2= (Button)findViewById(R.id.button2);
+        btn5= (Button)findViewById(R.id.button5);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Ini Tombol 1",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Ini Tombol 2",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Ini Tombol INFO",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
